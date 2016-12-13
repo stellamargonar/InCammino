b93985276bd13cdf4a9341312fdeea6747be35db
Stella Margonar <stella.margonar@unitn.it>
2016-12-13 12:31:06 +0100

    Integrated leaflet map with gpx layer

--- src/pages/stageMap/stageMap.ts
+++ src/pages/stageMap/stageMap.ts
@@ -1,15 +1,37 @@
 import { Component } from '@angular/core';
 
 import { NavParams } from 'ionic-angular';
+import * as Leaflet from 'leaflet';
+import * as Omnivore from 'leaflet-omnivore';
+import { Geolocation } from 'ionic-native';
+
 
 @Component({  
     templateUrl: 'stageMap.html',
 })
 export class stageMap {
 
-  stage : {};
+  stage : any;
+  stagePosition : any;
+  private _map: any;
+  private _currentLatLng: any;
+  
 
   constructor(public navParams: NavParams) {
+    // stage must have a map property containing:
+    // - name/url of the gpx file
+    // - coordinates for centering the map  : [latitude, longitude]
+    // - zoom level : 13
     this.stage = navParams.data;
+    console.log(this.stage);
+    this.stagePosition = this.stage.position;
+  }
+  ionViewDidEnter() {
+    this.loadMap();
+  }
+  loadMap() {
+    this._map = Leaflet.map("map").setView(this.stagePosition.coordinates, this.stagePosition.zoom);
+    Leaflet.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}').addTo(this._map);
+    Omnivore.gpx('data/' + this.stagePosition.trackFile).addTo(this._map);
   }
 }
